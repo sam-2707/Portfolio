8b47699c5c2b7c1918cdb83eb807272db10597d6
Sam <krishnsameer54@gmail.com>
2025-07-17 09:54:30 +0530

    Fifth

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
